6b830329ab39ecf51f6953b649bf3bdea74f73b6
Dominik Marti <dom@inik.ch>
2016-06-29 10:47:59 +0200

    Updated camp to include all the tissues in makeTissueList

--- private/makecmap.m
+++ private/makecmap.m
@@ -1,24 +1,23 @@
 function cmap = makecmap(Nt)
 % function cmap = makecmap(Nt)
-%   Currently, set for makecmap(Nt), where Nt <= 7.
+%   Currently, set for makecmap(Nt), where Nt <= 11.
 %   You can add more colors as you add tissues to makeTissueList.m.
 
 cmap = zeros(64,3);
-f1  = 0.7; % color adjustments
-f2  = 0.5; % color adjustments
-f3  = 0.3; % color adjustments
 
 dj = 0.05;
 for i=1:64
     j = round((i-dj)/64*(Nt-1));
-    if      j<=1-dj, cmap(i,:) = [0 0 0]; 
-    elseif  j<=2-dj, cmap(i,:) = [1 1 1];
-    elseif  j<=3-dj, cmap(i,:) = [1 0 0];
-    elseif  j<=4-dj, cmap(i,:) = [1 0.8 0.8];
-    elseif  j<=5-dj, cmap(i,:) = [0.5 0.2 0.2];
-    elseif  j<=6-dj, cmap(i,:) = [f1 1 f1]; % skull
-    elseif  j<=7-dj, cmap(i,:) = [f2 f2 f2]; % gray matter
+    if      j<=1-dj, cmap(i,:) = [0 0 0]; % escape
+    elseif  j<=2-dj, cmap(i,:) = [1 1 1]; % air
+    elseif  j<=3-dj, cmap(i,:) = [1 0 0]; % blood
+    elseif  j<=4-dj, cmap(i,:) = [1 0.8 0.8]; % dermis
+    elseif  j<=5-dj, cmap(i,:) = [0.5 0.2 0.2]; % epidermis
+    elseif  j<=6-dj, cmap(i,:) = [0.7 1 0.7]; % skull
+    elseif  j<=7-dj, cmap(i,:) = [0.5 0.5 0.5]; % gray matter
     elseif  j<=8-dj, cmap(i,:) = [0.5 1 1]; % white matter
+    elseif  j<=9-dj, cmap(i,:) = [0.3 .2 .2]; % hair
+    elseif  j<=9-dj, cmap(i,:) = [0 0 1]; % water
     elseif  j<=9-dj, cmap(i,:) = [1 .5 .5]; % standard tissue
     end
 end
