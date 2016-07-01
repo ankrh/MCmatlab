cf01d3a28390a975e06e5acbc1410bf7f5bf74e9
Dominik Marti <dom@inik.ch>
2016-07-01 14:18:19 +0200

    Bugfixes in plotTissue

--- private/plotTissue.m
+++ private/plotTissue.m
@@ -4,29 +4,21 @@ Nt = length(tissueList);
 Nx = length(x);
 xmin = min(x);
 xmax = max(x);
-dx = (xmax-xmin)/Nx;
 Nz = length(z);
 zmin = min(z);
 zmax = max(z);
 zdiff = zmax-zmin;
-dz = zdiff/Nz;
 
 imagesc(x,z,T,[1 Nt])
-set(gca,'fontsize',18)
 xlabel('x [cm]')
 ylabel('z [cm]')
-colorbar('YTickLabel',{tissueList.name},'YTick',1:Nt);
+axis([xmin xmax zmin zmax])
+axis equal image
 cmap = makecmap(Nt);
 colormap(cmap)
-set(colorbar,'fontsize',1)
-
-for i=1:Nt
-    yy = (Nt-i)/(Nt-1)*Nz*dz;
-    text(Nx*dx*1.2,yy, tissueList(i).name,'fontsize',12)
-end
+colorbar('YTickLabel',{tissueList.name},'YTick',1:Nt);
+set(gca,'fontsize',18)
 
 text(xmax*0.9,zmin - zdiff*0.06, 'Tissue types','fontsize',18)
-axis equal image
-axis([xmin xmax zmin zmax])
 
 return
