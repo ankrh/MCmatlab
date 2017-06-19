24650250b159413aa2908e6203006c6ed2b5a986
Anders <akh@norlase.com>
2017-06-19 16:00:03 +0200

    Adjusted plotTissueProperties so it will now plot only optical properties if the thermal properties are absent or incomplete in the tissueList struct (which is defined in makeTissueList). Users should now be able to disregard all thermal-related parts of the code if they only want to do optical simulations.

--- private/plotTissueProperties.m
+++ private/plotTissueProperties.m
@@ -3,40 +3,50 @@ function plotTissueProperties(tissueList)
 nT = length(tissueList);
 cmap = colormap(hsv(nT));
 
-subplot(231);
+plotThermalProperties = (isfield(tissueList,'VHC') && isfield(tissueList,'TC') && length([tissueList.VHC]) == nT && length([tissueList.TC]) == nT);
+
+if plotThermalProperties
+    rows = 2;
+else
+    rows = 1;
+end
+
+subplot(rows,3,1);
 hold on;
 for i=1:nT
     bar(i,tissueList(i).mua,'FaceColor',cmap(i,:));
 end
 set(gca,'XTick',1:nT,'XTickLabel',{tissueList.name},'XTickLabelRotation',45,'FontSize',12);
 title('\mu_a [cm^{-1}]');
-subplot(232);
+subplot(rows,3,2);
 hold on;
 for i=1:nT
     bar(i,tissueList(i).mus,'FaceColor',cmap(i,:));
 end
 set(gca,'XTick',1:nT,'XTickLabel',{tissueList.name},'XTickLabelRotation',45,'FontSize',12);
 title('\mu_s [cm^{-1}]');
-subplot(233);
+subplot(rows,3,3);
 hold on;
 for i=1:nT
     bar(i,tissueList(i).g,'FaceColor',cmap(i,:));
 end
 set(gca,'XTick',1:nT,'XTickLabel',{tissueList.name},'XTickLabelRotation',45,'FontSize',12);
 title('g');
-subplot(234);
-hold on;
-for i=1:nT
-    bar(i,tissueList(i).VHC,'FaceColor',cmap(i,:));
-end
-set(gca,'XTick',1:nT,'XTickLabel',{tissueList.name},'XTickLabelRotation',45,'FontSize',12);
-title('VHC [J/(cm^3*K)]');
-subplot(235);
-hold on;
-for i=1:nT
-    bar(i,tissueList(i).TC,'FaceColor',cmap(i,:));
-end
-set(gca,'XTick',1:nT,'XTickLabel',{tissueList.name},'XTickLabelRotation',45,'FontSize',12);
-title('TC [W/(cm*K)]');
 
+if plotThermalProperties
+    subplot(rows,3,4);
+    hold on;
+    for i=1:nT
+        bar(i,tissueList(i).VHC,'FaceColor',cmap(i,:));
+    end
+    set(gca,'XTick',1:nT,'XTickLabel',{tissueList.name},'XTickLabelRotation',45,'FontSize',12);
+    title('VHC [J/(cm^3*K)]');
+    subplot(rows,3,5);
+    hold on;
+    for i=1:nT
+        bar(i,tissueList(i).TC,'FaceColor',cmap(i,:));
+    end
+    set(gca,'XTick',1:nT,'XTickLabel',{tissueList.name},'XTickLabelRotation',45,'FontSize',12);
+    title('TC [W/(cm*K)]');
+end
 end
