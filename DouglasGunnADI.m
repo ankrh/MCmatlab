44e884d10670d699cf8ad4c75ca8ea3aa2bb576c
Anders Kragh Hansen home laptop <ankrh@fotonik.dtu.dk>
2019-03-25 00:44:57 +0100

    WIP

--- DouglasGunnADI.m
+++ DouglasGunnADI.m
@@ -1,57 +1,204 @@
+calctype = 2; % 1: 1D, 2:2D, 3:3D
+
 insulatingboundary = false;
 
-dt = 0.01*rand;
+dt = 0.0001;
 
-dx = rand;
-dy = 0.09;
+dx = 0.1;
+dy = 0.05;
 dz = 0.08;
 
-TC = [10 10];
+TC = [10 ; 1];
 
-VHC = [1 1];
+VHC = [1 ; 2];
 
 nx = 30;
-nt = 1000;
-M   = ones(1,nx).';
-ABS = zeros(1,nx).';
-ABS(20) = 1;
+ny = 40;
+nz = 30;
+nt = 500;
 
 %% Calculate matrices used for Douglas-Gunn ADI method
-TCmat = [0 TC(M) 0].';
-VHCmat = VHC(M).';
+x = dx*(-(nx-1)/2:(nx-1)/2);
+y = dy*(-(ny-1)/2:(ny-1)/2);
+switch calctype
+    case 1
+        N = nx;
+        M   = ones(nx,1);
+        M(12:13) = 2;
+        ABS = zeros(nx,1); % Power deposited per unit length/area/volume per unit time
+        ABS(10) = 100;
 
-effectiveTCx_mat = 2*TCmat(1:end-1,:,:).*TCmat(2:end,:,:)./(TCmat(1:end-1,:,:)+TCmat(2:end,:,:));
-effectiveTCy_mat = 2*TCmat(:,1:end-1,:).*TCmat(:,2:end,:)./(TCmat(:,1:end-1,:)+TCmat(:,2:end,:));
-effectiveTCz_mat = 2*TCmat(:,:,1:end-1).*TCmat(:,:,2:end)./(TCmat(:,:,1:end-1)+TCmat(:,:,2:end));
-effectiveTCx_mat(isnan(effectiveTCx_mat)) = 0; % Neighboring insulating voxels would return NaN but should just be 0
-effectiveTCy_mat(isnan(effectiveTCy_mat)) = 0;
-effectiveTCz_mat(isnan(effectiveTCz_mat)) = 0;
+        A1_full = eye(N);
+        B_full = eye(N);
+        B_rhs = zeros(N,1);
+        for i=1:N
+            ix = i;
+            insulatedboundaryvoxel = insulatingboundary && (ix == 1 || ix == nx);
+            if ix ~= 1 && ix ~= nx
+                TC_eff_posx = 2*TC(M(i))*TC(M(i+1))/(TC(M(i))+TC(M(i+1)));
+                if isnan(TC_eff_posx)
+                    TC_eff_posx = 0;
+                end
+                TC_eff_negx = 2*TC(M(i))*TC(M(i-1))/(TC(M(i))+TC(M(i-1)));
+                if isnan(TC_eff_negx)
+                    TC_eff_negx = 0;
+                end
+                A1_full(i,i)   = A1_full(i,i)   + dt*(TC_eff_posx + TC_eff_negx)/(2*VHC(M(i))*dx^2);
+                A1_full(i,i-1) = A1_full(i,i-1) - dt*TC_eff_negx /(2*VHC(M(i))*dx^2);
+                A1_full(i,i+1) = A1_full(i,i+1) - dt*TC_eff_posx /(2*VHC(M(i))*dx^2);
+                B_full(i,i)   = B_full(i,i)   - dt*(TC_eff_posx + TC_eff_negx)/(2*VHC(M(i))*dx^2);
+                B_full(i,i-1) = B_full(i,i-1) + dt*TC_eff_negx /(2*VHC(M(i))*dx^2);
+                B_full(i,i+1) = B_full(i,i+1) + dt*TC_eff_posx /(2*VHC(M(i))*dx^2);
+            elseif insulatedboundaryvoxel
+                if ix == 1
+                    TC_eff_posx = 2*TC(M(i))*TC(M(i+1))/(TC(M(i))+TC(M(i+1)));
+                    if isnan(TC_eff_posx)
+                        TC_eff_posx = 0;
+                    end
+                    A1_full(i,i)   = A1_full(i,i)   + dt*TC_eff_posx/(2*VHC(M(i))*dx^2);
+                    A1_full(i,i+1) = A1_full(i,i+1) - dt*TC_eff_posx /(2*VHC(M(i))*dx^2);
+                    B_full(i,i)   = B_full(i,i)   - dt*TC_eff_posx/(2*VHC(M(i))*dx^2);
+                    B_full(i,i+1) = B_full(i,i+1) + dt*TC_eff_posx /(2*VHC(M(i))*dx^2);
+                else
+                    TC_eff_negx = 2*TC(M(i))*TC(M(i-1))/(TC(M(i))+TC(M(i-1)));
+                    if isnan(TC_eff_negx)
+                        TC_eff_negx = 0;
+                    end
+                    A1_full(i,i)   = A1_full(i,i)   + dt*TC_eff_negx /(2*VHC(M(i))*dx^2);
+                    A1_full(i,i-1) = A1_full(i,i-1) - dt*TC_eff_negx /(2*VHC(M(i))*dx^2);
+                    B_full(i,i)   = B_full(i,i)   - dt*TC_eff_negx /(2*VHC(M(i))*dx^2);
+                    B_full(i,i-1) = B_full(i,i-1) + dt*TC_eff_negx /(2*VHC(M(i))*dx^2);
+                end
+            end
+            if ~insulatedboundaryvoxel
+                B_rhs(i) = ABS(i)*dt/VHC(M(i));
+            end
+        end
+        A1 = sparse(A1_full);
+        B = sparse(B_full);
 
-Ax_lhs = sparse(numel(M),numel(M));
-Ax_lhs(nx+1:nx+1:end) = -effectiveTCx_mat(2:end-1)/(2*dx^2);
-Ax_lhs(   1:nx+1:end) = VHCmat/dt + (effectiveTCx_mat(1:end-1) + effectiveTCx_mat(2:end))/(2*dx^2);
-Ax_lhs(   2:nx+1:end) = -effectiveTCx_mat(2:end-1)/(2*dx^2);
+        T = zeros(nx,1);
+        T([1 end]) = 0;
+        figure(1);clf;
+        h_line = plot(T);
+        % ylim([0 1.5]);
+        for i=1:nt
+            T = A1\(B*T + B_rhs);
+            h_line.YData = T;
+            pause(0.1);
+        end
+        
+        
+    case 2
+        N = nx*ny;
+        M = ones(nx,ny);
+        M(5:15,23:30) = 2;
+        ABS = zeros(nx,ny); % Power deposited per unit length/area/volume per unit time
+        ABS(8,22) = 100;
+        
+        A1_full     = zeros(N);
+        A2_full     = zeros(N);
+        B_full      = eye(N);
+        s           = zeros(N,1);
+        for i=1:N
+            [ix,iy] = ind2sub([nx,ny],i);
+            insulatedboundaryvoxel = insulatingboundary && (ix == 1 || iy == 1 || ix == nx || iy == ny);
+            
+            %% x couplings
+            if ix ~= 1 && ix ~= nx
+                TC_eff_posx = 2*TC(M(i))*TC(M(i+1))/(TC(M(i))+TC(M(i+1)));
+                TC_eff_negx = 2*TC(M(i))*TC(M(i-1))/(TC(M(i))+TC(M(i-1)));
+                A1_full(i,i)   = A1_full(i,i)   + dt*(TC_eff_posx + TC_eff_negx)/(2*VHC(M(i))*dx^2);
+                A1_full(i,i-1) = A1_full(i,i-1) - dt*TC_eff_negx /(2*VHC(M(i))*dx^2);
+                A1_full(i,i+1) = A1_full(i,i+1) - dt*TC_eff_posx /(2*VHC(M(i))*dx^2);
+                B_full(i,i)    = B_full(i,i  )  - dt*(TC_eff_posx + TC_eff_negx)/(2*VHC(M(i))*dx^2);
+                B_full(i,i-1)  = B_full(i,i-1)  + dt*TC_eff_negx /(2*VHC(M(i))*dx^2);
+                B_full(i,i+1)  = B_full(i,i+1)  + dt*TC_eff_posx /(2*VHC(M(i))*dx^2);
+            elseif insulatedboundaryvoxel
+                if ix == 1
+                    TC_eff_posx = 2*TC(M(i))*TC(M(i+1))/(TC(M(i))+TC(M(i+1)));
+                    A1_full(i,i)   = A1_full(i,i)   + dt*TC_eff_posx/(2*VHC(M(i))*dx^2);
+                    A1_full(i,i+1) = A1_full(i,i+1) - dt*TC_eff_posx /(2*VHC(M(i))*dx^2);
+                    B_full(i,i)    = B_full(i,i  )  - dt*(TC_eff_posx + TC_eff_negx)/(2*VHC(M(i))*dx^2);
+                    B_full(i,i+1)  = B_full(i,i+1)  + dt*TC_eff_posx /(2*VHC(M(i))*dx^2);
+                else
+                    TC_eff_negx = 2*TC(M(i))*TC(M(i-1))/(TC(M(i))+TC(M(i-1)));
+                    A1_full(i,i)   = A1_full(i,i)   + dt*TC_eff_negx /(2*VHC(M(i))*dx^2);
+                    A1_full(i,i-1) = A1_full(i,i-1) - dt*TC_eff_negx /(2*VHC(M(i))*dx^2);
+                    B_full(i,i)    = B_full(i,i  )  - dt*(TC_eff_posx + TC_eff_negx)/(2*VHC(M(i))*dx^2);
+                    B_full(i,i-1)  = B_full(i,i-1)  + dt*TC_eff_negx /(2*VHC(M(i))*dx^2);
+                end
+            end
+            
+            %% y couplings
+            if iy ~= 1 && iy ~= ny
+                TC_eff_posy = 2*TC(M(i))*TC(M(i+nx))/(TC(M(i))+TC(M(i+nx)));
+                TC_eff_negy = 2*TC(M(i))*TC(M(i-nx))/(TC(M(i))+TC(M(i-nx)));
+                A2_full(i,i)    = A2_full(i,i)    + dt*(TC_eff_posy + TC_eff_negy)/(2*VHC(M(i))*dy^2);
+                A2_full(i,i-nx) = A2_full(i,i-nx) - dt*TC_eff_negy /(2*VHC(M(i))*dy^2);
+                A2_full(i,i+nx) = A2_full(i,i+nx) - dt*TC_eff_posy /(2*VHC(M(i))*dy^2);
+                B_full(i,i)     = B_full(i,i   )  - dt*(TC_eff_posy + TC_eff_negy)/(2*VHC(M(i))*dy^2);
+                B_full(i,i-nx)  = B_full(i,i-nx)  + dt*TC_eff_negy /(2*VHC(M(i))*dy^2);
+                B_full(i,i+nx)  = B_full(i,i+nx)  + dt*TC_eff_posy /(2*VHC(M(i))*dy^2);
 
-Ax_rhs = sparse(numel(M),numel(M));
-Ax_rhs(nx+1:nx+1:end) = effectiveTCx_mat(2:end-1)/(2*dx^2);
-Ax_rhs(   1:nx+1:end) = VHCmat/dt - (effectiveTCx_mat(1:end-1) + effectiveTCx_mat(2:end))/(2*dx^2);
-Ax_rhs(   2:nx+1:end) = effectiveTCx_mat(2:end-1)/(2*dx^2);
+                Ay_lhs_full(i,i)    = Ay_lhs_full(i,i)    + dt*(TC_eff_posy + TC_eff_negy)/(2*VHC(M(i))*dy^2);
+                Ay_lhs_full(i,i-1)  = Ay_lhs_full(i,i-1)  - dt*TC_eff_negy /(2*VHC(M(i))*dy^2); % To be used after permuting x and y
+                Ay_lhs_full(i,i+1)  = Ay_lhs_full(i,i+1)  - dt*TC_eff_posy /(2*VHC(M(i))*dy^2); % To be used after permuting x and y
+            elseif insulatedboundaryvoxel
+                if iy == 1
+                    TC_eff_posy = 2*TC(M(i))*TC(M(i+1))/(TC(M(i))+TC(M(i+1)));
+                    A2_full(i,i)    = A2_full(i,i)    + dt*TC_eff_posy/(2*VHC(M(i))*dy^2);
+                    A2_full(i,i+nx) = A2_full(i,i+nx) - dt*TC_eff_posy/(2*VHC(M(i))*dy^2);
+                    B_full(i,i)     = B_full(i,i   )  - dt*TC_eff_posy/(2*VHC(M(i))*dy^2);
+                    B_full(i,i+nx)  = B_full(i,i+nx)  + dt*TC_eff_posy/(2*VHC(M(i))*dy^2);
 
-B_rhs = ABS;
+                    Ay_lhs_full(i,i)    = Ay_lhs_full(i,i)    + dt*TC_eff_posy/(2*VHC(M(i))*dy^2);
+                    Ay_lhs_full(i,i+nx) = Ay_lhs_full(i,i+nx) - dt*TC_eff_posy /(2*VHC(M(i))*dy^2);
+                else
+                    TC_eff_negy = 2*TC(M(i))*TC(M(i-1))/(TC(M(i))+TC(M(i-1)));
+                    A2_full(i,i)    = A2_full(i,i)    + dt*TC_eff_negy/(2*VHC(M(i))*dy^2);
+                    A2_full(i,i-nx) = A2_full(i,i-nx) - dt*TC_eff_negy/(2*VHC(M(i))*dy^2);
+                    B_full(i,i)     = B_full(i,i   )  - dt*TC_eff_negy/(2*VHC(M(i))*dy^2);
+                    B_full(i,i-nx)  = B_full(i,i-nx)  + dt*TC_eff_negy/(2*VHC(M(i))*dy^2);
 
-if ~insulatingboundary
-    Ax_lhs([1 nx+1 end-nx end]) = [1 0 0 1];
-    Ax_rhs([1 nx+1 end-nx end]) = [1 0 0 1];
-    B_rhs([1 end])              = [0 0];
+                    Ay_lhs_full(i,i)    = Ay_lhs_full(i,i)    + dt*TC_eff_negy/(2*VHC(M(i))*dy^2);
+                    Ay_lhs_full(i,i-nx) = Ay_lhs_full(i,i-nx) - dt*TC_eff_negy /(2*VHC(M(i))*dy^2);
+                end
+            end
+            
+            %% Absorption
+            if ~insulatedboundaryvoxel
+                s(i) = ABS(i)*dt/VHC(M(i));
+            end
+        end
+        A1 = sparse(A1_full);
+        A2 = sparse(A2_full);
+        B  = sparse(B_full);
+        step1_lhs = eye(N) + A1;
+        step1_rhs = - B - A2;
+        step2_lhs = eye(N) + A2;
+        step2_rhs = A2;
+        
+        figure(2);clf;
+        imagesc(M.');
+        axis equal
+        axis tight
+        axis xy
+        
+        T = zeros(nx*ny,1);
+        T([1 end]) = 0;
+        figure(1);clf;
+        h_im = imagesc(x,y,reshape(T,[nx ny]).');
+        colorbar;
+        axis equal
+        axis tight
+        axis xy
+%         caxis([0 0.2]);
+        for i=1:nt
+            v1 = A1\(B*T + s); % This is the intermediate temperature estimate
+            h_im.CData = reshape(T,[nx ny]).';
+            colorbar;
+            drawnow;
+        end
+        
 end
-
-T = rand(nx,1);
-T([1 end]) = 0;
-figure(1);clf;
-h_line = plot(T);
-for i=1:nt
-    T = Ax_lhs\(Ax_rhs*T + B_rhs);
-    h_line.YData = T;
-    pause(0.01);
-end
-
