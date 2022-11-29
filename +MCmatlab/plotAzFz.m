f95b81ac8d7804818aaa1ad4e3a7949b197775f7
Anders Kragh Hansen <ankrh@fotonik.dtu.dk>
2022-11-29 15:51:19 +0100

    Fixed R2018a compatibility for the plotAzFz() function.

--- +MCmatlab/plotAzFz.m
+++ +MCmatlab/plotAzFz.m
@@ -1,4 +1,4 @@
 function plotAzFz(model)
-  MCmatlab.NdimSliderPlot(model.G.dx*model.G.dy*sum(model.MC.NA,[1 2]),'nFig',41,'axisLabels',{'','','z [cm]','Az [cm^{-1}]'},'axisValues',{[],[],model.G.z});
-  MCmatlab.NdimSliderPlot(model.G.dx*model.G.dy*sum(model.MC.NFR,[1 2]),'nFig',42,'axisLabels',{'','','z [cm]','Fz [-]'},'axisValues',{[],[],model.G.z});
+  MCmatlab.NdimSliderPlot(model.G.dx*model.G.dy*sum(sum(model.MC.NA ,1),2),'nFig',41,'axisLabels',{'','','z [cm]','Az [cm^{-1}]'},'axisValues',{[],[],model.G.z});
+  MCmatlab.NdimSliderPlot(model.G.dx*model.G.dy*sum(sum(model.MC.NFR,1),2),'nFig',42,'axisLabels',{'','','z [cm]','Fz [-]'},'axisValues',{[],[],model.G.z});
 end
